ba3adaf2bb56e2bbd1f9095cda475f1336d4faed
WiseMango13452 <rabbitcreator2@gmail.com>
2023-07-05 00:08:06 +0200

    Items

--- src/main/java/mango/winteroverhaul/item/ModItems.java
+++ src/main/java/mango/winteroverhaul/item/ModItems.java
@@ -2,21 +2,22 @@ package mango.winteroverhaul.item;
 
 import mango.winteroverhaul.WinterOverhaul;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    public static final Item ExplosiveSnowball = registerItem("Explosive Snowball",
-            new Item(new FabricItemSettings()));
-
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, new Identifier(WinterOverhaul.MOD_ID, name), item);
-    }
+    public static final Item ExplosiveSnowball = new Item(new FabricItemSettings());
 
     public static void registerModItems() {
         WinterOverhaul.LOGGER.debug("Registering Mod Items for " + WinterOverhaul.MOD_ID);
+        Registry.register(Registries.ITEM, new Identifier("winter_overhaul", "explosive_snowball"), ExplosiveSnowball);
     }
+
 }
