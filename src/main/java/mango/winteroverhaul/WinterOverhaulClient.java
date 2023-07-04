e24eb03bfa244018dba9afcdf109fb6f9536226b
WiseMango13452 <rabbitcreator2@gmail.com>
2023-07-04 23:13:33 +0200

    Initial commit

---
+++ src/main/java/mango/winteroverhaul/WinterOverhaulClient.java
@@ -0,0 +1,10 @@
+package mango.winteroverhaul;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WinterOverhaulClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
